730c10277f31cf2b81d1d8bbf9dbff621fa7f5cf
Patrick Armstrong <pat@oldpatricka.com>
2018-07-14 21:42:56 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,68 @@
+# kecret - The Kubernetes Secret Editor
+
+This little app helps you with editing secrets when working with Kubernetes.
+It decodes all base64 encoded values in your secret and automatically opens
+$EDITOR, then automatically re-encodes them when you save your file and exit
+the editor. Handy!
+
+## Installation
+
+```
+go get github.com/oldpatricka/kecret
+```
+
+or get the binary:
+
+```
+TKTKTK
+```
+
+## Usage
+
+Simply use kecret as your editor to edit your secret file like so:
+
+```
+kecret mysecret.yaml
+```
+
+Your editor will display the decoded secret values which you can edit with ease.
+When you save, your secret file will automatically be re-encoded.
+
+## Motivation
+
+Why? Well, my regular workflow when working with secrets in Kubernetes is to
+keep [SOPS](https://github.com/mozilla/sops) encrypted files in my source
+repository, and decrypt them and apply them every time I make a change to the
+secrets. This means my full workflow whenever I need to edit a secret is:
+
+```
+sops --decrypt mysecret.enc.yaml > mysecret.yaml
+$EDITOR mysecret.yaml
+<copy secret>
+pbpaste | base64 -D
+<copy decoded secret into another editor>
+<edit secret>
+<copy edited secret>
+pbpaste | base64 | pbcopy
+$EDITOR mysecret.yaml
+<paste secret in the right location>
+<save>
+sops --encrypt mysecret.yaml > mysecret.enc.yaml
+```
+
+This is pretty annoying, and kecret helps with this a bit, by changing this
+workflow to:
+
+```
+sops --decrypt mysecret.enc.yaml > mysecret.yaml
+kecret mysecret.yaml
+<edit secret>
+<save>
+sops --encrypt mysecret.yaml > mysecret.enc.yaml
+```
+
+Better! Less error prone!
+
+```
+
+
---
+++ main.go
@@ -0,0 +1,156 @@
+package main
+
+import (
+	"fmt"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"os"
+	"encoding/base64"
+	"os/exec"
+)
+
+func usage() string {
+	return fmt.Sprintf("%s FILENAME", os.Args[0])
+}
+
+type Secret struct {
+	ApiVersion string `yaml:"apiVersion"`
+	Data       map[string]string
+	Kind       string
+	Metadata   map[string]string
+	Type       string
+}
+
+func exitWithError(msg string) {
+	exitWithMessage(msg, 1)
+}
+
+func exitWithMessage(msg string, code int) {
+	fmt.Fprintf(os.Stderr, msg)
+	os.Exit(code)
+}
+
+func encodeSecret(es Secret) {
+	for k, d := range es.Data {
+		v := base64.StdEncoding.EncodeToString([]byte(d))
+		es.Data[k] = v
+	}
+}
+
+func saveBytesToTempFile(decoded []byte) string {
+	tempFile, err := ioutil.TempFile("", "kecret")
+	if err != nil {
+		msg := fmt.Sprintf("Couldn't create temporary file: %s", err)
+		exitWithError(msg)
+	}
+	tempFileName := tempFile.Name()
+	tempFile.Write(decoded)
+	tempFile.Close()
+	return tempFileName
+}
+
+func decodeSecretFile(secretFileName string) (Secret, error) {
+
+	s := Secret{}
+
+	rawSecret, err := ioutil.ReadFile(secretFileName)
+	if err != nil {
+		return s, fmt.Errorf("couldn't read secret file: %s", err)
+	}
+
+	err = yaml.Unmarshal(rawSecret, &s)
+	if err != nil {
+		return s, fmt.Errorf("couldn't unmarshal secret file: %s", err)
+	}
+
+	for k, v := range s.Data {
+		d, err := base64.StdEncoding.DecodeString(v)
+		if err != nil {
+			return s, fmt.Errorf("couldn't base64 decode secret %s: %s", k, err)
+		}
+		s.Data[k] = string(d)
+	}
+	return s, nil
+}
+
+func editFile(tempFileName string) error {
+	editor := os.Getenv("EDITOR")
+	if editor == "" {
+		editor = "vi" // same thing kubectl does
+	}
+	cmd := exec.Command(editor, tempFileName)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err := cmd.Start()
+	if err != nil {
+		return fmt.Errorf("couldn't start editor %s: %s", editor, err)
+	}
+	err = cmd.Wait()
+	if err != nil {
+		return fmt.Errorf("error while editing: %s", err)
+	}
+	return nil
+}
+
+func main() {
+
+
+	if len(os.Args) != 2 {
+		exitWithError(usage())
+	}
+
+	secretFileName := os.Args[1]
+	s, err := decodeSecretFile(secretFileName)
+	if err != nil {
+		exitWithError(err.Error())
+	}
+
+	decoded, err := yaml.Marshal(&s)
+	if err != nil {
+		msg := fmt.Sprintf("Couldn't marshal yaml: %s", err)
+		exitWithError(msg)
+	}
+
+	tempFileName := saveBytesToTempFile(decoded)
+	if err != nil {
+		exitWithError(err.Error())
+	}
+
+	err = editFile(tempFileName)
+	if err != nil {
+		exitWithError(err.Error())
+	}
+
+	editedRawSecret, err := ioutil.ReadFile(tempFileName)
+	if err != nil {
+		msg := fmt.Sprintf("Couldn't read edited secret: %s", err)
+		exitWithError(msg)
+		return
+	}
+
+	if string(editedRawSecret) == string(decoded) {
+		exitWithMessage("No change to secret, aborting...", 0)
+	}
+
+	es := Secret{}
+	yaml.Unmarshal(editedRawSecret, &es)
+	if err != nil {
+		msg := fmt.Sprintf("Couldn't unmarshal edited secret: %s", err)
+		exitWithError(msg)
+	}
+
+	encodeSecret(es)
+
+	encoded, err := yaml.Marshal(&es)
+	if err != nil {
+		msg := fmt.Sprintf("Couldn't marshal edited secret: %s", err)
+		exitWithError(msg)
+	}
+
+	err = ioutil.WriteFile(secretFileName, encoded, 0)
+	if err != nil {
+		msg := fmt.Sprintf("Couldn't write edited secret: %s", err)
+		exitWithError(msg)
+	}
+}
